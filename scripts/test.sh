e1d2a6869ce69317128268e63fadbfc4abefb4d9
Alex Baldwin <alewin@noc.ac.uk>
2025-12-12 18:04:40 +0000

    Removing accidentally uploaded test output

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ cd $SCRIPT_DIR
 cd ..
 bash scripts/get-test-data.sh
 mkdir -p testout
-python3 src/main.py ifcb_to_ecotaxa -i testdata/*.hdr --operator Testname --project "PAP" --ship "James Cook" --depth 5 --tableonly -o ecotaxa_test.tsv
+python3 src/main.py ifcb_to_ecotaxa -i testdata/*.hdr --operator Testname --project "PAP" --ship "James Cook" --depth 5 --tableonly -o testout/ecotaxa_test.tsv
